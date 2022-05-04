23cad839ead105505ef4881c2f3e5637827cf503
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-05-04 14:35:20 +0400

    Modified copy_setup.sh to be more helpful

--- copy_setup.sh
+++ copy_setup.sh
@@ -6,5 +6,6 @@ RESET="\033[0m"
 SETUP_FILES=$(command find /home/hsarhan/42 -name "Makefile_with*" -o -type d -name "c_unit")
 
 cp -r $SETUP_FILES .
+rm ./c_unit/Makefile ./c_unit/tests.c
 
 echo -e "${PURPLE}Copied setup files:\n${BLUE}${SETUP_FILES}${RESET}"
