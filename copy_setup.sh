037ba67ab575e5264d07b367879bd46f4c28e3ba
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-05-07 08:06:52 +0400

    Fix bug in copy_setup.sh

--- copy_setup.sh
+++ copy_setup.sh
@@ -3,7 +3,8 @@ PURPLE="\033[0;35m"
 BLUE="\033[0;34m"
 RESET="\033[0m"
 
-SETUP_FILES=$(command find /home/hsarhan/42 -name "Makefile_with*" -o -type d -name "c_unit")
+SETUP_FILES=$(command find "$HOME/42" -name "Makefile_with*")
+SETUP_FILES="$HOME/42/c_unit $SETUP_FILES"
 
 cp -r $SETUP_FILES .
 rm ./c_unit/Makefile ./c_unit/tests.c
