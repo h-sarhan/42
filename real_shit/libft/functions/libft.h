ecae56e60ab4bbdcb3ac8fffdf8e1e5d2a6700eb
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-05-10 15:21:25 +0400

    Add ft_calloc implementation

--- real_shit/libft/functions/libft.h
+++ real_shit/libft/functions/libft.h
@@ -6,13 +6,15 @@
 /*   By: hsarhan <hassanAsarhan@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 10:23:07 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/05/10 10:16:23 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/05/10 15:11:01 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -28,4 +30,5 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+void	*ft_calloc(size_t nmemb, size_t size);
 #endif
