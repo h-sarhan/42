d8dc273231f7094fa751bdbe30a77043544f7563
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-05-15 14:34:29 +0400

    Uncomment some tests

--- real_shit/libft/functions/tests/run_tests.c
+++ real_shit/libft/functions/tests/run_tests.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hassanAsarhan@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 15:15:22 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/05/15 13:44:14 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/05/15 14:33:02 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,44 +27,44 @@ int	main(int argc, char **argv)
 		test_ft_putnbr_fd(atoi(argv[2]));
 	else
 	{
-	//	print_msg_color("\n===Testing libc ascii functions===\n", MAGENTA);
-	//	test_isalpha();
-	//	test_isdigit();
-	//	test_isalnum();
-	//	test_isascii();
-	//	test_isprint();
+		print_msg_color("\n===Testing libc ascii functions===\n", MAGENTA);
+		test_isalpha();
+		test_isdigit();
+		test_isalnum();
+		test_isascii();
+		test_isprint();
 	
-//		print_msg_color("\n===Testing libc string functions===\n", MAGENTA);
-//		test_strlen();
-	//	test_toupper();
-	//	test_tolower();
-	//	test_strchr();
-//		test_strrchr();
-//		test_strdup();
-//		test_strncmp();
-//		test_strnstr();
-//	  	test_atoi();
-	//  	test_strlcpy(); // <---- TEST HARDER (DONE)
-	//	test_strlcat(); // <---- TEST HARDER (DONE)
+		print_msg_color("\n===Testing libc string functions===\n", MAGENTA);
+		test_strlen();
+		test_toupper();
+		test_tolower();
+		test_strchr();
+		test_strrchr();
+		test_strdup();
+		test_strncmp();
+		test_strnstr();
+	  	test_atoi();
+	  	test_strlcpy(); // <---- TEST HARDER (DONE)
+		test_strlcat(); // <---- TEST HARDER (DONE)
 	
-//		print_msg_color("\n===Testing libc memory functions===\n", MAGENTA);
-//		test_memset();
-//		test_bzero();
-//  	test_calloc();
-//		test_memchr();
-//		test_memcmp();
-//		test_memcpy(); 
-//		test_memmove(); // <---- TEST HARDER (DONE)
-//	
-//		print_msg_color("\n===Testing additional functions===\n", MAGENTA);
-//		test_ft_substr(); // <--- TEST HARDER (DONE)
-//		test_ft_strjoin(); // <--- TEST HARDER (DONE)
-//		test_ft_strtrim();
-//  	test_ft_split();
-//		test_ft_itoa();
-//		test_ft_strmapi();
-//		test_ft_striteri();
-//	  
+		print_msg_color("\n===Testing libc memory functions===\n", MAGENTA);
+		test_memset();
+		test_bzero();
+  		test_calloc();
+		test_memchr();
+		test_memcmp();
+		test_memcpy(); 
+		test_memmove(); // <---- TEST HARDER (DONE)
+	
+		print_msg_color("\n===Testing additional functions===\n", MAGENTA);
+		test_ft_substr(); // <--- TEST HARDER (DONE)
+		test_ft_strjoin(); // <--- TEST HARDER (DONE)
+		test_ft_strtrim();
+  		test_ft_split();
+		test_ft_itoa();
+		test_ft_strmapi();
+		test_ft_striteri();
+	  
 		print_msg_color("\n===Testing bonus functions===\n", MAGENTA);
 		test_ft_lstnew();
 		test_ft_lstadd_front();
