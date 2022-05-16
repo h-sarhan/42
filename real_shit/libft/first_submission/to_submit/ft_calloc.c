c66f89d3b5e6075fc8c80517467b62b8acbaf419
Hassan Sarhan <hsarhan@lab3r4s3.42abudhabi.ae>
2022-05-16 09:33:30 +0400

    Create to_submit folder

---
+++ real_shit/libft/first_submission/to_submit/ft_calloc.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsarhan <hassanAsarhan@outlook.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 15:00:08 by hsarhan           #+#    #+#             */
+/*   Updated: 2022/05/15 12:28:33 by hsarhan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned char	*memory;
+	size_t			i;
+
+	if (nmemb == 0 || size == 0 || nmemb > SIZET_MAX / size)
+		return (NULL);
+	memory = malloc(nmemb * size);
+	if (memory == NULL)
+	{
+		errno = ENOMEM;
+		return (NULL);
+	}
+	i = 0;
+	while (i < nmemb * size)
+	{
+		memory[i] = 0;
+		i++;
+	}
+	return (memory);
+}
---
+++ real_shit/libft/first_submission/to_submit/ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsarhan <hassanAsarhan@outlook.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 13:44:47 by hsarhan           #+#    #+#             */
+/*   Updated: 2022/05/13 13:44:43 by hsarhan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*bytes1;
+	const unsigned char	*bytes2;
+	size_t				i;
+
+	bytes1 = s1;
+	bytes2 = s2;
+	i = 0;
+	while (i < n)
+	{
+		if (bytes1[i] > bytes2[i])
+			return (1);
+		else if (bytes1[i] < bytes2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
