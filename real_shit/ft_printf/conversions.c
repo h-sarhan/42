d4d2d8d5c9ac9ce252ddc3fdb0b68dc097d7b3fa
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-05-18 23:18:56 +0400

    Fix some edge cases with printing hex characters

--- real_shit/ft_printf/conversions.c
+++ real_shit/ft_printf/conversions.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hassanAsarhan@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 00:38:19 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/05/18 17:28:04 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/05/18 23:15:39 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
@@ -50,13 +50,15 @@ void	print_hex(unsigned long num, int print_upper)
 	while (i >= 0)
 	{
 		left = bytes[i] >> 4;
-		right = bytes[i] & 0x0f;
-		if (left != 0 || right != 0 || start == TRUE)
+		if (left != 0 || start == TRUE)
 		{
-			if (left != 0 || start == TRUE)
-				ft_putchar_fd(dec_to_hex(left, print_upper), STDOUT);
-			if (right != 0 || start == TRUE)
-				ft_putchar_fd(dec_to_hex(right, print_upper), STDOUT);
+			ft_putchar_fd(dec_to_hex(left, print_upper), STDOUT);
+			start = TRUE;
+		}
+		right = bytes[i] & 0x0f;
+		if (right != 0 || start == TRUE)
+		{
+			ft_putchar_fd(dec_to_hex(right, print_upper), STDOUT);
 			start = TRUE;
 		}
 		i--;
@@ -65,11 +67,21 @@ void	print_hex(unsigned long num, int print_upper)
 
 void	print_hex_pointer(void *pointer)
 {
-	ft_putstr_fd("0x", STDOUT);
-	print_hex((long unsigned) pointer, LOWER);
+	ft_putstr_fd("FT_PRINTF: ", STDOUT);
+	if (pointer == NULL)
+		ft_putstr_fd("(null)", STDOUT);
+	else
+	{
+		ft_putstr_fd("0x", STDOUT);
+		print_hex((unsigned long) pointer, LOWER);
+	}
 }
 
 void	print_hex_int(unsigned int num, int print_upper)
 {
-	print_hex(num, print_upper);
+	ft_putstr_fd("FT_PRINTF: ", STDOUT);
+	if (num == 0)
+		ft_putchar_fd('0', STDOUT);
+	else
+		print_hex(num, print_upper);
 }
--- real_shit/ft_printf/tests.c
+++ real_shit/ft_printf/tests.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hassanAsarhan@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 00:54:05 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/05/18 17:29:30 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/05/18 23:13:18 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,24 @@
 
 int	main(int argc, char **argv)
 {
-	print_hex(atoi(argv[1]), UPPER);
-	printf("\n%X\n", atoi(argv[1]));
+	print_hex_int(atoi(argv[1]), UPPER);
+	printf("\nOG_PRINTF: %X\n", atoi(argv[1]));
 	int	n = 45;
 	int *p = &n;
-	print_hex_pointer(p);
+	print_hex_pointer(45);
 	write(STDOUT, "\n", 1);
-	printf("%p\n", p);
+	printf("OG_PRINTF: %p\n", 45);
+	
+	
+	print_hex_int(4294967295 + 20, UPPER);
+	printf("\nOG_PRINTF: %X\n", 4294967295 + 20);
+	
+	print_hex_pointer(4294967295 + 20);
+	printf("\nOG_PRINTF: %p\n", 4294967295 + 20);
+	
+	print_hex_int(0, UPPER);
+	printf("\nOG_PRINTF: %X\n", 0);
+	
+	print_hex_pointer(0);
+	printf("\nOG_PRINTF: %p\n", 0);
 }
