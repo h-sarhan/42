fcd00dc8eff62d9c17721f7b346897d6d958d862
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-05-27 23:12:42 +0400

    Prepare mandatory part

--- real_shit/ft_printf/sub00/bonus/print_str_conversions_bonus.c
+++ real_shit/ft_printf/sub00/bonus/print_str_conversions_bonus.c
@@ -1,50 +1,64 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_str_conversions.c                            :+:      :+:    :+:   */
+/*   print_str_conversions_bonus.c                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:50:37 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/05/27 22:55:51 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/05/27 22:37:51 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../ft_printf.h"
 
 // Prints a string conversion
+// Handles the following flags
+// 1. Minimum width
+// 2. Left adjustment '-'
+// 3. Precision
 int	print_string_conversion(t_conversion *conv, char *val)
 {
 	int		num_printed;
 	char	null_str[20];
 	int		i;
 
-	if (conv->type != 's')
-		return (-1);
 	if (val == NULL)
 	{
 		ft_strlcpy(null_str, "(null)", 20);
 		val = null_str;
 	}
-	num_printed = 0;
+	calculate_padding(conv, &val);
+	if (!conv->pad_right)
+		print_n_chars(' ', conv->mw_padding);
+	num_printed = conv->mw_padding;
 	i = 0;
-	while (val[i] != '\0')
+	while (val[i] != '\0' && (i < conv->precision_amount || !conv->precision))
 	{
 		ft_putchar_fd(val[i], STDOUT);
 		i++;
 		num_printed++;
 	}
+	if (conv->pad_right)
+		print_n_chars(' ', conv->mw_padding);
 	return (num_printed);
 }
 
 // Prints a character conversion
+// Handles the following flags
+// 1. Minimum width
+// 2. Left adjustment '-'
 int	print_char_conversion(t_conversion *conv, char val)
 {
 	int	num_printed;
 
-	if (conv->type != 'c' && conv->type != '%')
-		return (-1);
+	calculate_padding(conv, &val);
+	if (!conv->pad_right)
+		print_n_chars(' ', conv->mw_padding);
+	num_printed = conv->mw_padding;
 	ft_putchar_fd(val, STDOUT);
-	num_printed = 1;
+	num_printed += 1;
+	if (conv->pad_right)
+		print_n_chars(' ', conv->mw_padding);
 	return (num_printed);
 }
