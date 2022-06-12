3d39789aba07b07b4883532ca49bb79e6a198b15
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-06-13 00:25:17 +0400

    yep

--- cursus/pipex/sub00/process_playlist/main.c
+++ cursus/pipex/sub00/process_playlist/main.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+
+int	get_exit_status(int status)
+{
+	return ((status & 0xff00) >> 8);
+}
 int	main(int argc, char *argv[])
 {
 	int pid = fork();
@@ -12,11 +20,14 @@ int	main(int argc, char *argv[])
 	}
 	if (pid == 0)
 	{
-		execlp("ping", "ping", "-c", "3", "google.com", NULL);
+		execlp("which", "which", "pig", NULL);
 	}
 	else
 	{
-		wait(NULL);
+		int status;
+		wait(&status);
+		printf("EXIT STATUS IS %d\n", get_exit_status(status));
+		printf("EXIT STATUS 2 IS %d\n", WEXITSTATUS(status));
 		printf("Success!\n");
 	}
 	return (0);
