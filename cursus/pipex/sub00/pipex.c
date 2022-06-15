3e602d161cae83f3fa91cc9297d52d97368ff3b6
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-06-15 19:41:49 +0400

    Hadi showed me stuff

--- cursus/pipex/sub00/pipex.c
+++ cursus/pipex/sub00/pipex.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 13:42:13 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/06/15 16:28:03 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/06/15 19:40:28 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,17 +57,28 @@ int	main(int argc, char **argv)
 	char	*infile = argv[1];
 	char	**cmd_1_args = ft_split(argv[2], ' ');
 	char	*cmd_1 = cmd_1_args[0];
+	// char	**cmd_2_args = ft_split(argv[3], ' ');
+	// char	*cmd_2 = cmd_2_args[0];f
 
+	// int	pipe_fds[2];
+	// pipe(pipe_fds);
 	if (access(cmd_1_args[0], F_OK) == -1)
 	{
 		cmd_1_args[0] = get_full_path(cmd_1_args[0]);
 		free(cmd_1);
 	}
+	// if (access(cmd_2_args[0], F_OK) == -1)
+	// {
+	// 	cmd_2_args[0] = get_full_path(cmd_2_args[0]);
+	// 	free(cmd_2);
+	// }
 	int	in_fd = open(infile, O_RDONLY);	
 	int pid = fork();
 	if (pid == 0)
 	{
+		// close(pipe_fds[READ]);
 		dup2(in_fd, 0);
+		// dup2(pipe_fds[WRITE], 1);
 		execve(cmd_1_args[0], cmd_1_args, NULL);
 	}
 	else
@@ -75,4 +86,17 @@ int	main(int argc, char **argv)
 		close(in_fd);
 		waitpid(pid, NULL, 0);
 	}
+	// char *buff = ft_calloc(1001, sizeof(char));
+	// pid = fork();
+	// if (pid == 0)
+	// {
+		// close(pipe_fds[WRITE]);
+		// read(pipe_fds[READ], buff, 1001);
+	// 	execve(cmd_2_args[0], cmd_2_args, NULL);
+	// }
+	// else
+	// {
+	// 	// printf("BUFFER IS: %s\n", buff);
+	// 	waitpid(pid, NULL, 0);
+	// }
 }
