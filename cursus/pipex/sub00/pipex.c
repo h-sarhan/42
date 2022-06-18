4853510a7a551f70180b4ad854c6b2060caa4715
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-06-18 17:06:15 +0400

    Add nice comments

--- cursus/pipex/sub00/pipex.c
+++ cursus/pipex/sub00/pipex.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 13:42:13 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/06/18 16:44:27 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/06/18 17:05:37 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,6 @@ int main(int argc, char **argv, char **env)
 	int		devnull_fd;
 	int		cmd_1_valid;
 	int		cmd_2_valid;
-	int		exit_code;
 	int		w_status;
 	int		in_fd;
 	int		out_fd;
@@ -56,7 +55,6 @@ int main(int argc, char **argv, char **env)
 	int		pid1;
 	int		pid2;
 
-	exit_code = 0;
 	if (argc != 5)
 	{
 		ft_putendl_fd("Wrong number of arguments", 2);
@@ -64,6 +62,7 @@ int main(int argc, char **argv, char **env)
 	}
 	
 	// checking infile
+	// check_file function
 	if (access(argv[1], R_OK) == -1)
 		in_fd = -1;
 	else
@@ -71,6 +70,7 @@ int main(int argc, char **argv, char **env)
 	fd_check(in_fd, argv[1]);
 	
 	// checking command 1
+	// check command_function
 	cmd_1_args = ft_split(argv[2], ' ');
 	malloc_check(cmd_1_args);
 	if (access(cmd_1_args[0], X_OK) == -1)
@@ -81,10 +81,12 @@ int main(int argc, char **argv, char **env)
 	cmd_1_valid = command_check(cmd_1_args, argv[2], NULL, in_fd);
 
 	// checking outfile
+	// check_file function
 	out_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	fd_check(out_fd, argv[4]);
 	
 	// checking command 2
+	// check command_function
 	cmd_2_args = ft_split(argv[3], ' ');
 	malloc_check(cmd_2_args);
 	if (access(cmd_2_args[0], X_OK) == -1)
@@ -92,12 +94,14 @@ int main(int argc, char **argv, char **env)
 		cmd_2_args[0] = get_full_path(cmd_2_args[0], env);
 		malloc_check(cmd_2_args[0]);
 	}
-	cmd_2_valid = command_check(cmd_2_args, argv[3], &exit_code, out_fd);
+	cmd_2_valid = command_check(cmd_2_args, argv[3], NULL, out_fd);
 
-
+	// Create pipe function
 	pipe_check(pipe(pipe_fds));
 	devnull_fd = open("/dev/null", O_RDONLY);
 	fd_check(devnull_fd, "/dev/null");
+	
+	// fork_command function
 	pid1 = -1;
 	if (cmd_1_valid)
 	{
@@ -105,6 +109,8 @@ int main(int argc, char **argv, char **env)
 		fork_check(pid1);
 	}
 
+
+	// fork command function
 	if (pid1 == 0)
 	{
 		close_fd(pipe_fds[READ]);
@@ -129,14 +135,13 @@ int main(int argc, char **argv, char **env)
 	{
 		if (in_fd != -1)
 			close_fd(in_fd);
-		// waitpid(pid1, NULL, 0);
-		// if (cmd_1_valid && in_fd != -1)
+
+		// fork_command function
 		pid2 = -1;
 		if (cmd_2_valid)
 		{
 			pid2 = fork();
 			fork_check(pid2);
-
 		}
 		if (pid2 == 0)
 		{
@@ -146,6 +151,7 @@ int main(int argc, char **argv, char **env)
 			if (cmd_2_valid && out_fd != -1)
 				execve(cmd_2_args[0], cmd_2_args, env);
 			// if execve fails do the below
+			// cleanup
 			close_fd(pipe_fds[READ]);
 			close_fd(out_fd);
 			close_fd(devnull_fd);
@@ -155,6 +161,7 @@ int main(int argc, char **argv, char **env)
 		}
 		else
 		{
+			// cleanup
 			close_fd(pipe_fds[WRITE]);
 			close_fd(pipe_fds[READ]);
 			close_fd(out_fd);
@@ -165,6 +172,8 @@ int main(int argc, char **argv, char **env)
 				waitpid(pid2, &w_status, 0);
 			free_split_array(cmd_1_args);
 			free_split_array(cmd_2_args);
+			if (!cmd_2_valid)
+				exit(127);
 			exit((w_status >> 8) & 0x000000ff);
 		}
 	}
