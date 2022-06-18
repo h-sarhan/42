a5dcab075a3fd85b715d0958c35930435facdaf4
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-06-18 08:11:31 +0400

    Add error handling for invalid command

--- cursus/pipex/sub00/error_handling.c
+++ cursus/pipex/sub00/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 17:52:13 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/06/17 18:21:02 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/06/18 08:05:15 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	malloc_check(void *mem)
 {
-	if (mem == NULL)
+	if (mem == NULL && errno == ENOMEM)
 	{
 		perror("pipex");
 		exit(EXIT_FAILURE);
@@ -27,6 +27,24 @@ void	fd_check(int fd, char *file_name)
 	{
 		ft_putstr_fd("pipex: no such file or directory: ", 2);
 		ft_putendl_fd(file_name, 2);
+	}
+}
+
+void command_check(char *cmd_path, char *cmd_args)
+{
+	char	**args;
+	char	*cmd_name;
+	
+	if (cmd_path == NULL && errno == ENOENT)
+	{
+		args = ft_split(cmd_args, ' ');
+		malloc_check(args);
+		cmd_name = ft_strdup(args[0]);
+		malloc_check(cmd_name);
+		free_split_array(args);
+		// TODO: Write "command not found" message instead
+		perror(cmd_name);
+		ft_free(cmd_name);
 	}
 }
 
