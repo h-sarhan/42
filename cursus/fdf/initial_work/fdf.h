f2f72330dc31a476bbbc0505c5d43a5dd459a02a
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-06-25 20:52:39 +0400

    Add boilerplate code for fdf

---
+++ cursus/fdf/initial_work/fdf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 20:35:32 by hsarhan           #+#    #+#             */
+/*   Updated: 2022/06/25 20:37:50 by hsarhan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include "libft/libft.h"
+# include "get_next_line/get_next_line.h"
+# include "printf/ft_printf.h"
+#endif
