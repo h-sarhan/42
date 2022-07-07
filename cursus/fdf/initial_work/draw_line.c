d6ccd1783ce1ee937263c3bf2dd27716e4263108
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-07-07 18:36:07 +0400

    Norminetted draw_line.c

--- cursus/fdf/initial_work/draw_line.c
+++ cursus/fdf/initial_work/draw_line.c
@@ -6,108 +6,96 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 17:47:37 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/07/07 12:40:37 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/07/07 18:35:39 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-
-void draw_line_low(t_data *img, t_point *p1, t_point *p2, t_vars *vars)
+void	draw_line_low(t_data *img, t_point *p1, t_point *p2, t_vars *vars)
 {
-	int dx;
-	int dy;
-	int yi;
-	int d;
-	int y;
-	int x;
-	int color;
+	int	dxy[2];
+	int	yi;
+	int	d;
+	int	xy[2];
+	int	color;
 
-	dx = p2->x - p1->x;
-	dy = p2->y - p1->y;
+	dxy[0] = p2->x - p1->x;
+	dxy[1] = p2->y - p1->y;
 	yi = 1;
-	if (dy < 0)
+	if (dxy[1] < 0)
 	{
 		yi = -1;
-		dy = -dy;
+		dxy[1] = -dxy[1];
 	}
-	d = (2 * dy) - dx;
-	y = p1->y;
-	x = p1->x;
-	
+	d = (2 * dxy[1]) - dxy[0];
+	xy[1] = p1->y;
+	xy[0] = p1->x;
 	color = p1->color;
-	while (x < p2->x)
+	while (xy[0] < p2->x)
 	{
 		if (p2->color == p1->color)
-			my_mlx_pixel_put(img, x, y, p1->color, vars);
+			my_mlx_pixel_put(img, xy[0], xy[1], p1->color, vars);
 		else
 		{
-			// color = add_color(color, color1_diff);
-			// color = add_color(color, color2_diff);
-			color = color_mix(p1->color, p2->color, (float)(x - p1->x) / (p2->x - p1->x));
-			my_mlx_pixel_put(img, x, y, color, vars);
+			color = color_mix(p1->color, p2->color,
+					(float)(xy[0] - p1->x) / (p2->x - p1->x));
+			my_mlx_pixel_put(img, xy[0], xy[1], color, vars);
 		}
 		if (d > 0)
 		{
-			y = y + yi;
-			d = d + (2 * (dy - dx));
+			xy[1] = xy[1] + yi;
+			d = d + (2 * (dxy[1] - dxy[0]));
 		}
 		else
-			d = d + 2 * dy;
-		x++;
-		// i++;
+			d = d + 2 * dxy[1];
+		xy[0]++;
 	}
 }
 
-void draw_line_high(t_data *img, t_point *p1, t_point *p2, t_vars *vars)
+void	draw_line_high(t_data *img, t_point *p1, t_point *p2, t_vars *vars)
 {
-	int dx;
-	int dy;
-	int xi;
-	int d;
-	int y;
-	int x;
-	int color;
-	
+	int	dxy[2];
+	int	xi;
+	int	d;
+	int	xy[2];
+	int	color;
 
-	dx = p2->x - p1->x;
-	dy = p2->y - p1->y;
+	dxy[0] = p2->x - p1->x;
+	dxy[1] = p2->y - p1->y;
 	xi = 1;
-	if (dx < 0)
+	if (dxy[0] < 0)
 	{
 		xi = -1;
-		dx = -dx;
+		dxy[0] = -dxy[0];
 	}
-	d = (2 * dx) - dy;
-	x = p1->x;
-	y = p1->y;
+	d = (2 * dxy[0]) - dxy[1];
+	xy[0] = p1->x;
+	xy[1] = p1->y;
 	color = p1->color;
-	while (y < p2->y)
+	while (xy[1] < p2->y)
 	{
 		if (p2->color == p1->color)
-			// color = p1->color;
-			my_mlx_pixel_put(img, x, y, p1->color, vars);
+			my_mlx_pixel_put(img, xy[0], xy[1], p1->color, vars);
 		else
 		{
-			// color = add_color(color, color1_diff);
-			// color = add_color(color, color2_diff);
-			color = color_mix(p1->color, p2->color, (float)(y - p1->y) / (p2->y - p1->y));
-			my_mlx_pixel_put(img, x, y, color, vars);
+			color = color_mix(p1->color, p2->color,
+					(float)(xy[1] - p1->y) / (p2->y - p1->y));
+			my_mlx_pixel_put(img, xy[0], xy[1], color, vars);
 		}
 		if (d > 0)
 		{
-			x = x + xi;
-			d = d + (2 * (dx - dy));
+			xy[0] = xy[0] + xi;
+			d = d + (2 * (dxy[0] - dxy[1]));
 		}
 		else
-			d = d + 2 * dx;
+			d = d + 2 * dxy[0];
 		y++;
 	}
 }
 
-void draw_line(t_data *img, t_point *p1, t_point *p2, t_vars *vars)
+void	draw_line(t_data *img, t_point *p1, t_point *p2, t_vars *vars)
 {
-	// printf("Drawing from (%d,%d) to (%d,%d)\n", p1->x, p1->y, p1->x, p2->y);
 	if (fabs(p2->y - p1->y) < fabs(p2->x - p1->x))
 	{
 		if (p1->x > p2->x)
@@ -121,5 +109,5 @@ void draw_line(t_data *img, t_point *p1, t_point *p2, t_vars *vars)
 			draw_line_high(img, p2, p1, vars);
 		else
 			draw_line_high(img, p1, p2, vars);
-	} 
+	}
 }
