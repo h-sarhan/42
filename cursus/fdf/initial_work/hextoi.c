3016c128642e2b7b1ff4b39f3b3107c3b7da94af
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-07-07 18:38:35 +0400

    Norminneted hextoi.c

--- cursus/fdf/initial_work/hextoi.c
+++ cursus/fdf/initial_work/hextoi.c
@@ -6,31 +6,31 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 18:57:54 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/06/30 22:05:50 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/07/07 18:37:14 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static int is_space(char c)
+static int	is_space(char c)
 {
 	return (c == ' ' || c == '\f' || c == '\n'
-			 || c == '\r' || c == '\t' || c == '\v');
+		|| c == '\r' || c == '\t' || c == '\v');
 }
 
-unsigned int hextoi(char *str)
+unsigned int	hextoi(char *str)
 {
 	unsigned int	num;
 	int				i;
 
 	num = 0;
 	i = 0;
-	while(is_space(str[i]))
+	while (is_space(str[i]))
 		i++;
 	if (str[i] == '0' && (str[i + 1] == 'x' || str[i + 1] == 'X'))
 		i += 2;
 	while ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'a' && str[i] <= 'f')
-			|| (str[i] >= 'A' && str[i] <= 'F'))
+		|| (str[i] >= 'A' && str[i] <= 'F'))
 	{
 		num *= 16;
 		if (str[i] >= '0' && str[i] <= '9')
